68dba75cb0168da1e73e847ad0a5ee2398538433
Ian-Acosta2330 <ian2330acosta@gmail.com>
2024-02-06 17:34:52 -0700

    wrote function for 8x12 font

--- draw_chars.c
+++ draw_chars.c
@@ -1,5 +1,5 @@
 #include "draw.h"		/* for font */
-#include "stdio.h"		/* for putchar */
+#include <stdio.h>		/* for putchar */
 
 void print_char_11x16(char c)
 {
@@ -13,3 +13,17 @@ void print_char_11x16(char c)
     putchar('\n');
   }
 }
+
+void print_char_8x12(char c)
+{
+  c-= 0x20;
+  for (char row = 0; row < 10; row++) {
+    unsigned short rowBits = font_8x12[c][row];
+    for (char col = 0; col < 12; col++) {
+      unsigned short colMask = 1 << (11-col);
+      putchar( (rowBits & colMask) ? '*' : ' ');
+    }
+    putchar('\n');
+    }
+}
+
